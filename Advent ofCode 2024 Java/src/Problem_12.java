2e61387d3bf5d0b3340ffa4413bf4605bf08aa5b
AaravY1508 <aaravy1508@gmail.com>
2025-01-01 03:26:59 +0530

    problem 12
    Just check the number of corners from problem 12 part 2 part 1 is easy just add perimeter whenever teh flood fill goes out of the region

--- Advent ofCode 2024 Java/src/Problem_12.java
+++ Advent ofCode 2024 Java/src/Problem_12.java
@@ -32,24 +32,47 @@ public class Problem_12 {
         //     System.out.println();
         // }
         boolean[][] visited = new boolean[lines.length][lines.length];
+        int sum = 0;
         for (int x = 0; x < visited.length; x++) {
             for (int y = 0; y < visited.length; y++) {
                 if(visited[x][y] == false){
-                    floodFill(x, y, map, visited);
+                    int[] perimeter = {0};
+                    int[] area = {0};
+                    floodFill(x, y, map, visited, area, perimeter, map[x][y], 0, 0);
+                    sum+=area[0]*perimeter[0];
+                    // System.out.println(area[0]*perimeter[0]);
                 }
             }
         }
+        System.out.println(sum);
         // floodFill(0, 0, map, visited);
     }
-    public static int floodFill(int x, int y, char[][] map, boolean[][] visited){
-        if(x < 0 || x >= map.length) return -1;
-        if(y < 0 || y >= map.length) return -1;
+    public static int floodFill(int x, int y, char[][] map, boolean[][] visited, int[] area, int[] perimeter, char current, int vx, int vy){
+        x+=vx; y+=vy;
+        // System.out.println(x + " " + y); 
+        if(x < 0 || x >= map.length || y < 0 || y >= map.length || map[x][y] != current) { // if it exits the region some really condensed maths i don't want to finger wave calculate again
+            int wx = x-vy-vx;
+            int wy = y-vy+vx;
+            int x2 = x-vy;
+            int y2 = y+vx;
+            // char test = map[x2][y2];
+            if(wx < 0 || wx >= map.length || wy < 0 || wy >= map.length || map[wx][wy] != current) {
+                perimeter[0]++;
+            }
+            else if(x2 >= 0 && x2 < map.length && y2 >= 0 && y2 < map.length && map[x2][y2] == current) {
+                // System.out.println(map[x2][y2]);
+                perimeter[0]++;
+            }
+            return -1;
+        }
         if(visited[x][y]) return -1;
-
-        floodFill(x, y+1, map, visited);
-        floodFill(x+1, y, map, visited);
-        floodFill(x, y-1, map, visited);
-        floodFill(x-1, y, map, visited);
+        visited[x][y] = true;
+        area[0]++;
+        
+        floodFill(x, y, map, visited, area, perimeter, current, 0, 1);
+        floodFill(x, y, map, visited, area, perimeter, current, 1, 0);
+        floodFill(x, y, map, visited, area, perimeter, current, 0, -1);
+        floodFill(x, y, map, visited, area, perimeter, current, -1, 0);
         return -1;
     }
 }
